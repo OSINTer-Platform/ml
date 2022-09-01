f5a380b55f561c133dcb1dae1b40e757402f3cc2
bertmad3400 <skrivtilbertram@gmail.com>
2022-09-01 11:35:45 +0200

    Also scan for clusters among articles using HDBScan

--- cluster.py
+++ cluster.py
@@ -2,10 +2,11 @@ import umap
 import numpy as np
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-
 from scipy.spatial import distance
 
-from modules import config, elastic
+from hdbscan import HDBSCAN
+
+from modules import config, elastic, objects
 
 config_options = config.BackendConfig()
 
@@ -17,20 +18,17 @@ def generate_embeddings(data):
     return tfidf_embedding.embedding_
 
 def main(articles, numberOfNearest):
-    data = []
-
-    for article in articles:
-        article.similar = []
-        data.append(article.content)
+    data = [article.content for article in articles]
 
     embeddings = generate_embeddings(data)
-
     distances = distance.squareform(distance.pdist(embeddings))
-
     closest = np.argsort(distances, axis=1)[:, 1:numberOfNearest+1]
 
+    clusters = HDBSCAN(min_cluster_size=10, min_samples=5).fit(embeddings).labels_
+
     for i,article in enumerate(articles):
-        article.similar = [articles[point].id for point in closest[i]]
+        article.ml = { "similar" : [articles[point].id for point in closest[i]], "cluster" : clusters[i] }
+
         config_options.es_article_client.save_document(article)
 
 if __name__ == "__main__":
