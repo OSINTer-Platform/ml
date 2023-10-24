a231195fe1fadc127ab59f5dd6bd72d5d9e6af7a
bertmad3400 <skrivtilbertram@gmail.com>
2023-10-24 14:43:23 +0000

    Configure logger and setup needed folder upon startup

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,12 @@
 from dotenv import load_dotenv
 
-from modules.config import BaseConfig
+from modules.config import BaseConfig, configure_logger
+from modules.misc import create_folder
+
+create_folder("logs")
+create_folder("models")
 
 load_dotenv()
 
 config_options = BaseConfig()
+configure_logger("osinter")
