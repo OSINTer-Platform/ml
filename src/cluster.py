97e8c69e990b6e2de1340637325d30cc5379ed3a
bertmad3400 <skrivtilbertram@gmail.com>
2023-10-24 14:43:09 +0000

    Add code for clustering articles

---
+++ src/cluster.py
@@ -0,0 +1,195 @@
+from typing import Any, Callable, cast
+from nptyping import NDArray
+
+import logging
+from hashlib import md5
+
+import multiprocessing
+from concurrent.futures import ThreadPoolExecutor
+
+from modules.objects import FullArticle, Cluster
+from .inference import extract_labeled, query_openai, construct_description_prompts
+
+from bertopic import BERTopic
+from sentence_transformers import SentenceTransformer
+from umap import UMAP
+from hdbscan import HDBSCAN
+from sklearn.feature_extraction.text import CountVectorizer
+
+logger = logging.getLogger("osinter")
+
+umap_model = UMAP(
+    n_neighbors=15, n_components=5, min_dist=0.0, metric="cosine", random_state=42
+)
+hdbscan_model = HDBSCAN(
+    min_cluster_size=10,
+    metric="euclidean",
+    cluster_selection_method="eom",
+    prediction_data=True,
+)
+vectorizer_model = CountVectorizer(stop_words="english", min_df=2, ngram_range=(1, 2))
+embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+
+
+class ClusterCounter:
+    def __init__(self) -> None:
+        self.lock = multiprocessing.Manager().Lock()
+        self.count = 0
+
+    def get_count(self):
+        with self.lock:
+            self.count += 1
+            return self.count
+
+
+def describe_cluster(
+    keywords: list[str], representative_docs: list[str], use_openai: bool = True
+) -> tuple[str, str, str]:
+    # Defaults in case openai return nonsensical answer
+    title = " | ".join([keyword.capitalize() for keyword in keywords])
+    description = " | ".join([keyword.capitalize() for keyword in keywords])
+    summary = ""
+    default_response = title, description, summary
+
+    if not use_openai:
+        return default_response
+
+    prompts, labels = construct_description_prompts(keywords, representative_docs)
+    openai_response = query_openai(prompts)
+
+    if openai_response is None:
+        logger.warn(f'Response for cluster with title "{title}" failed to query openai')
+        return default_response
+
+    extracted = extract_labeled(openai_response, labels)
+
+    if extracted is None:
+        logger.warn(
+            f'Unable to extract details from OpenAI response with labels "{" | ".join(labels)}". OpenAI response: {openai_response}'
+        )
+        return default_response
+
+    return extracted
+
+
+def fit_topic_model(articles: list[FullArticle]) -> BERTopic:
+    topic_model = BERTopic(
+        # Pipeline models
+        embedding_model=embedding_model,
+        umap_model=umap_model,
+        hdbscan_model=hdbscan_model,
+        vectorizer_model=vectorizer_model,
+        # Hyperparameters
+        top_n_words=10,
+        verbose=True,
+    )
+
+    logger.debug("Pre-calculating embeddings for articles")
+    contents = [article.content for article in articles]
+
+    embeddings = cast(
+        NDArray[Any, Any],
+        embedding_model.encode(contents, show_progress_bar=True, convert_to_numpy=True),
+    )
+
+    logger.debug("Fitting to BERTopic")
+    topic_model.fit_transform(contents, embeddings)
+    return topic_model
+
+
+def create_clusters(
+    articles: list[FullArticle],
+    save_model: None | str,
+    use_openai: bool = True,
+) -> list[Cluster]:
+    def create_cluster(
+        record: dict[str, Any], topic_numbers: list[int], counter: ClusterCounter
+    ) -> Cluster:
+        cluster_count = counter.get_count()
+        logger.debug(f"Starting processing of cluster nr {cluster_count}")
+
+        topic_docs: list[FullArticle] = []
+        representative_docs: list[FullArticle] = []
+
+        for topic, article in zip(topic_numbers, articles):
+            if topic == record["Topic"]:
+                topic_docs.append(article)
+
+            if article.content in record["Representative_Docs"]:
+                representative_docs.append(article)
+
+        title, description, summary = describe_cluster(
+            record["Representation"], record["Representative_Docs"], use_openai
+        )
+
+        logger.debug(f"Finished processing of cluster nr {cluster_count}")
+
+        return Cluster(
+            id=md5(str(record["Topic"]).encode("utf-8")).hexdigest(),
+            nr=record["Topic"],
+            document_count=record["Count"],
+            title=title,
+            description=description,
+            summary=summary,
+            keywords=record["Representation"],
+            representative_documents=[article.id for article in representative_docs],
+            documents={article.id for article in topic_docs},
+            dating={article.publish_date for article in topic_docs},
+        )
+
+    logger.debug("Fitting new model")
+    topic_model = fit_topic_model(articles)
+
+    if save_model:
+        logger.debug(f'Saving model to file "{save_model}"')
+        topic_model.save(save_model, serialization="pickle")
+
+    logger.debug("Normalizing and enriching cluster data")
+    counter = ClusterCounter()
+
+    records = topic_model.get_topic_info().to_dict("records")
+
+    # TODO: Check type of returned topics
+    topic_numbers = cast(list[int], topic_model.topics_)
+
+    for topic, article in zip(topic_numbers, articles):
+        article.ml["cluster"] = topic
+
+    handle_record: Callable[[dict[str, Any]], Cluster] = lambda record: create_cluster(
+        record, topic_numbers, counter
+    )
+    with ThreadPoolExecutor(max_workers=6) as executor:
+        clusters: list[Cluster] = list(executor.map(handle_record, records))
+
+    return clusters
+
+
+def cluster_new_articles(
+    articles: list[FullArticle], clusters: list[Cluster], saved_model: str
+) -> None:
+    def update_clusters(
+        clusters: list[Cluster], articles: list[FullArticle], topic_numbers: list[int]
+    ):
+        for cluster in clusters:
+            if cluster.nr not in topic_numbers:
+                continue
+
+            for topic, article in zip(topic_numbers, articles):
+                if topic == cluster.nr:
+                    cluster.documents.add(article.id)
+                    cluster.dating.add(article.publish_date)
+
+                    cluster.document_count = len(cluster.documents)
+
+    logger.debug(f'Loading model from file "{saved_model}"')
+
+    topic_model = BERTopic.load(saved_model)
+    topic_numbers, _ = topic_model.transform([article.content for article in articles])
+
+    # Despite typed differently, topic_numbers is numpy int64 and needs conversion
+    topic_numbers = [int(nr) for nr in topic_numbers]
+
+    for topic, article in zip(topic_numbers, articles):
+        article.ml["cluster"] = topic
+
+    update_clusters(clusters, articles, topic_numbers)
